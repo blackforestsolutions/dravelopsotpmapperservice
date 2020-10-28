4ecc7ac229b3993b649c058aa9c91ca5f6359795
jobdk <47722294+jobdk@users.noreply.github.com>
2020-10-28 10:43:50 +0100

    #patch

--- src/integrationtest/java/de/blackforestsolutions/dravelopsotpmapperservice/JourneyApiServiceIT.java
+++ src/integrationtest/java/de/blackforestsolutions/dravelopsotpmapperservice/JourneyApiServiceIT.java
@@ -5,12 +5,16 @@ import de.blackforestsolutions.dravelopsdatamodel.util.ApiToken;
 import de.blackforestsolutions.dravelopsotpmapperservice.service.communicationservice.JourneyApiService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.geo.Point;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
 import java.time.ZonedDateTime;
+import java.util.Locale;
 
 import static de.blackforestsolutions.dravelopsdatamodel.objectmothers.ApiTokenObjectMother.getOtpMapperApiToken;
 import static de.blackforestsolutions.dravelopsdatamodel.testutil.TestUtils.retrieveJsonToPojo;
@@ -20,12 +24,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class JourneyApiServiceIT {
 
-    @Autowired
-    private JourneyApiService classUnderTest;
-
     @Test
     void test_retrieveJourneysFromApiService_with_correct_apiToken_returns_results() {
-        ApiToken testData = getOtpMapperApiToken();
+        ApiToken testData = getCustomOtpMapperApiTokenFromResources();
         String jsonTestData = toJson(testData);
 
         Flux<String> result = classUnderTest.retrieveJourneysFromApiService(jsonTestData);
@@ -55,6 +56,25 @@ class JourneyApiServiceIT {
                 .verifyComplete();
     }
 
+
+    @Autowired
+    private JourneyApiService classUnderTest;
+
+    public ApiToken getCustomOtpMapperApiTokenFromResources() {
+        return (new ApiToken.ApiTokenBuilder())
+                .setProtocol(protocol)
+                .setHost(host)
+                .setPort(port)
+                .setPath(path)
+                .setOptimize(optimization)
+                .setIsArrivalDateTime(arrivalDateTime)
+                .setDateTime(ZonedDateTime.parse(dateTime))
+                .setDepartureCoordinate(new Point(departureCoordinates_x, departureCoordinates_y))
+                .setArrivalCoordinate(new Point(arrivalCoordinates_x, arrivalCoordinates_y))
+                .setLanguage(Locale.forLanguageTag(language))
+                .build();
+    }
+
     @Test
     void test_retrieveJourneysFromApiService_with_incorrect_apiToken_returns_zero_results() {
         ApiToken.ApiTokenBuilder testData = new ApiToken.ApiTokenBuilder(getOtpMapperApiToken());
@@ -67,5 +87,40 @@ class JourneyApiServiceIT {
         StepVerifier.create(result)
                 .expectNextCount(0L)
                 .verifyComplete();
+    }
+
+    /*@Value("$[{apiToken.protocol}")
+    private String protocol;
+    @Value("$[{apiToken.host}")
+    private String host;
+    @Value("$[{apiToken.port}")
+    private int port;
+    @Value("$[{apiToken.path}")
+    private String path;
+    @Value("$[{apiToken.optimize}")
+    private Optimization optimization;
+    @Value("$[{apiToken.arrivalDateTime}")
+    private boolean arrivalDateTime;
+    @Value("$[{apiToken.dateTime}")
+    private String dateTime;
+    @Value("$[{apiToken.departureCoordinates_x}")
+    private double departureCoordinates_x;
+    @Value("$[{apiToken.departureCoordinates_y}")
+    private double departureCoordinates_y;
+    @Value("$[{apiToken.arrivalCoordinates_x}")
+    private double arrivalCoordinates_x;
+    @Value("$[{apiToken.arrivalCoordinates_y}")
+    private double arrivalCoordinates_y;
+    @Value("$[{apiToken.language}")
+    private String language;*/
+    @TestConfiguration
+    static class TestConfig {
+
+        @Bean("test")
+        @ConfigurationProperties(prefix = "")
+        public ApiToken apiToken() {
+            return new ApiToken.ApiTokenBuilder().build();
+        }
+
     }
 }
